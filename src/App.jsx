926f277f8615cd922d0d3fdbe6b47cb0e63e1867
Laura-prog-ui <duarteriveralauraestefania0@gmail.com>
2023-03-17 10:39:34 -0500

    Feat: Cambios pequeños

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import ChangePassword from "./pages/login/ChangePassword";
 import LoginAdmin from "./pages/login/LoginAdmin";
 import LoginProfe from "./pages/login/LoginProfe";
-//import Alert from "./components/atoms/Alert"
+
 
 
 function App() {
 	return (
 		<div>
-			{/* <Alert/> */}
-			{/* <LoginProfe/> 
-			<LoginAdmin/> */}
+			<LoginProfe/> 
+			<LoginAdmin/>
 			<ChangePassword/>
 		</div>
 	);
